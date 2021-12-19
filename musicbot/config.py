b1a8de3ac349dabf953566fd49b6b3bc5d62caf4
fan9704 <cxz123499@gmail.com>
2021-12-19 21:29:45 +0800

    real

--- musicbot/config.py
+++ musicbot/config.py
@@ -37,7 +37,7 @@ class Config:
         self._confpreface2 = "An error has occured validating the config:\n"
 
         self._login_token = config.get(
-            "Credentials", "Token", int(os.environ.get('TOKEN'))
+            "Credentials", "Token", os.environ.get('TOKEN')
         )
 
         self.auth = ()
