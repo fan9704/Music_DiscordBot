f63ec19218fc60bd9ad67f06e5620a94240f7367
fan9704 <cxz123499@gmail.com>
2021-12-19 21:36:34 +0800

    fix fallback

--- musicbot/config.py
+++ musicbot/config.py
@@ -35,9 +35,9 @@ class Config:
 
         self._confpreface = "An error has occured reading the config:\n"
         self._confpreface2 = "An error has occured validating the config:\n"
-
+        print("[INFO] Discord Bot Token===>",os.environ['TOKEN'])
         self._login_token = config.get(
-            "Credentials", "Token", os.environ['TOKEN']
+            "Credentials", "Token", fallback=os.environ['TOKEN']
         )
 
         self.auth = ()
