58bfb89d118329a9ef7c651668e229107855ce81
fan9704 <cxz123499@gmail.com>
2021-12-24 17:52:02 +0800

    refresh

--- musicbot/player.py
+++ musicbot/player.py
@@ -330,7 +330,7 @@ class MusicPlayer(EventEmitter, Serializable):
                 self._source = SourcePlaybackCounter(
                     PCMVolumeTransformer(
                         FFmpegPCMAudio(
-                            executable="~/bin/ffmpeg.exe",
+                            executable="ffmpeg.exe",
                             source=entry.filename,
                             before_options=boptions,
                             options=aoptions,
